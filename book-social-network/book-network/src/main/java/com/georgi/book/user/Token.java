59d52e3103575d012e047f3557508f74f0c2d641
georgiivanov <gogata92@pm.me>
2025-02-21 13:20:05 +0100

    Created the token class and repository

---
+++ book-social-network/book-network/src/main/java/com/georgi/book/user/Token.java
@@ -0,0 +1,29 @@
+package com.georgi.book.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "token")
+public class Token {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String token;
+    private LocalDateTime createdAt;
+    private LocalDateTime expiresAt;
+    private LocalDateTime validatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+
+}
--- book-social-network/book-network/src/main/java/com/georgi/book/user/TokenRepository.java
+++ book-social-network/book-network/src/main/java/com/georgi/book/user/TokenRepository.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-
-    Optional<User> findByEmail(String email);
+public interface TokenRepository extends JpaRepository<Token, Long> {
+    Optional<Token> findByToken(String token);
 }
