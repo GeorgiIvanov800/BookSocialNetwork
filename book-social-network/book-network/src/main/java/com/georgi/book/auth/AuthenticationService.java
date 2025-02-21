d8fd5c564f3adec007edb48103852e3529436a90
georgiivanov <gogata92@pm.me>
2025-02-21 14:42:17 +0100

    Create Auth Service

---
+++ book-social-network/book-network/src/main/java/com/georgi/book/auth/AuthenticationService.java
@@ -0,0 +1,13 @@
+package com.georgi.book.auth;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class AuthenticationService {
+    public void register(RegistrationRequest request) {
+
+
+    }
+}
