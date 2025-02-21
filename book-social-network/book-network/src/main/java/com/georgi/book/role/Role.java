c72a1874ae42b557ec79e54e958bc85483cb91db
georgiivanov <gogata92@pm.me>
2025-02-21 13:15:48 +0100

    Created the repositories for the Role and User

---
+++ book-social-network/book-network/src/main/java/com/georgi/book/role/Role.java
@@ -0,0 +1,32 @@
+package com.georgi.book.role;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.georgi.book.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "role")
+@EntityListeners(AuditingEntityListener.class)
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    @JsonIgnore
+    private List<User> users;
+
+}
