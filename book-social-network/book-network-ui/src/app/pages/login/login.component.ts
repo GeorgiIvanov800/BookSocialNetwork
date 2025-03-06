fb31cd25c6e4ce64092f3251b39a03b66cf6c022
georgiivanov <gogata92@pm.me>
2025-03-06 18:43:41 +0100

    created login page

--- book-social-network/book-network-ui/src/app/pages/login/login.component.ts
+++ book-social-network/book-network-ui/src/app/pages/login/login.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
 import {AuthenticationRequest} from '../../services/models/authentication-request';
+import {NgForOf, NgIf} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {Router} from '@angular/router';
+import {AuthenticationService} from '../../services/services/authentication.service';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    NgIf,
+    NgForOf,
+    FormsModule,
+
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
@@ -11,4 +20,30 @@ export class LoginComponent {
 
   authRequest: AuthenticationRequest = {email: '', password: ''};
   errorMessage: Array<string> = [];
+
+  constructor(
+    private router: Router,
+    private authService: AuthenticationService,
+    // another service
+  ) {
+  }
+
+  login() {
+    this.errorMessage = [];
+    this.authService.authenticate({
+      body: this.authRequest
+    }).subscribe({
+      next: result => {
+        // todo save the token
+        this.router.navigate(['books']);
+      },
+      error: err => {
+        console.log(err);
+      }
+    })
+  }
+
+  register() {
+    this.router.navigate(['register']);
+  }
 }
