acaf3ec048d06648245b6b78e6b788b95fa7eec9
georgiivanov <gogata92@pm.me>
2025-03-16 16:46:52 +0100

    created dummy user for testing purposes

--- book-social-network/book-network/src/main/java/com/georgi/book/BookNetworkApiApplication.java
+++ book-social-network/book-network/src/main/java/com/georgi/book/BookNetworkApiApplication.java
@@ -2,12 +2,17 @@ package com.georgi.book;
 
 import com.georgi.book.role.Role;
 import com.georgi.book.role.RoleRepository;
+import com.georgi.book.user.User;
+import com.georgi.book.user.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.List;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
@@ -19,8 +24,26 @@ public class BookNetworkApiApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(RoleRepository roleRepository) {
+	public CommandLineRunner commandLineRunner(RoleRepository roleRepository, UserRepository userRepository, PasswordEncoder passwordEncoder) {
 		return args -> {
+			var userRole = roleRepository.findByName("USER")
+					//todo better exception handling
+					.orElseThrow(() -> new RuntimeException("User role not found"));
+
+			if(userRepository.count() == 0) {
+				userRepository.save(
+						User.builder()
+								.email("test@gmail.com")
+								.password(passwordEncoder.encode("test"))
+								.enabled(true)
+								.accountLocked(false)
+								.firstName("John")
+								.lastName("Doe")
+								.roles(List.of(userRole))
+								.build()
+				);
+			}
+
 			if (roleRepository.findByName("USER").isEmpty()) {
 				roleRepository.save(
 						Role.builder().name("USER").build()
