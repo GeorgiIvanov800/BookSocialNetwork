0e3785b5fb57d824835c9938ac97885c484f68c8
georgiivanov <gogata92@pm.me>
2025-03-16 19:39:07 +0100

    created manu component

---
+++ book-social-network/book-network-ui/src/app/pages/book/components/book-main/book-main/book-main.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {MenuComponent} from '../../menu/menu.component';
+
+@Component({
+  selector: 'app-book-main',
+  imports: [
+    MenuComponent
+  ],
+  templateUrl: './book-main.component.html',
+  styleUrl: './book-main.component.scss'
+})
+export class BookMainComponent {
+
+}
