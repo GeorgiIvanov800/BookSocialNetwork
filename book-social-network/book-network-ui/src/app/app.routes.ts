7bd88c432352df4e0641a6682e8a7f1874239312
georgiivanov <gogata92@pm.me>
2025-03-19 10:57:38 +0100

    created new book-list component

--- book-social-network/book-network-ui/src/app/app.routes.ts
+++ book-social-network/book-network-ui/src/app/app.routes.ts
@@ -1,12 +1,17 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 import {LoginComponent} from './pages/login/login.component';
 import {RegisterComponent} from './pages/register/register.component';
 import {ActivateAccountComponent} from './pages/activate-account/activate-account.component';
 import {BookMainComponent} from './pages/book/components/book-main/book-main/book-main.component';
+import {BookListComponent} from './pages/book/components/book-list/book-list.component';
 
 export const routes: Routes = [
-  { path: 'login', component: LoginComponent },
+  {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
-  {path:'activate-account', component: ActivateAccountComponent},
-  {path:'books', component: BookMainComponent},
+  {path: 'activate-account', component: ActivateAccountComponent},
+  {
+    path: 'books', component: BookMainComponent, children: [
+      {path: '', component: BookListComponent}
+    ]
+  },
 ];
--- book-social-network/book-network-ui/src/app/pages/book/components/book-main/book-main/book-main.component.ts
+++ book-social-network/book-network-ui/src/app/pages/book/components/book-main/book-main/book-main.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {MenuComponent} from '../../menu/menu.component';
+import {BookListComponent} from '../../book-list/book-list.component';
 
 @Component({
   selector: 'app-book-main',
   imports: [
-    MenuComponent
+    MenuComponent,
+    BookListComponent
   ],
   templateUrl: './book-main.component.html',
   styleUrl: './book-main.component.scss'
