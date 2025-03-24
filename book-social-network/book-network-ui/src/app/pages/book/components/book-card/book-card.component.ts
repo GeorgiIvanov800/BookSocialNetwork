023f88262bccb4a74a49b8e574493bd29e27305d
georgiivanov <gogata92@pm.me>
2025-03-24 20:29:39 +0100

    add method to emit from child to parent

--- book-social-network/book-network-ui/src/app/pages/book/components/book-card/book-card.component.ts
+++ book-social-network/book-network-ui/src/app/pages/book/components/book-card/book-card.component.ts
@@ -1,14 +1,20 @@
-import {Component, Input} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {BookResponse} from '../../../../services/models/book-response';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-book-card',
-  imports: [],
+  imports: [
+    NgIf
+  ],
   templateUrl: './book-card.component.html',
   styleUrl: './book-card.component.scss'
 })
 export class BookCardComponent {
+
   private _book: BookResponse = {};
+  private _manage = false
+
 
   get book(): BookResponse {
     return this._book;
@@ -19,4 +25,42 @@ export class BookCardComponent {
     this._book = value;
   }
 
+  get manage(): boolean {
+    return this._manage;
+  }
+  @Input()
+  set manage(value: boolean) {
+    this._manage = value;
+  }
+
+  @Output() private share: EventEmitter<BookResponse> = new EventEmitter<BookResponse>();
+  @Output() private archive: EventEmitter<BookResponse> = new EventEmitter<BookResponse>();
+  @Output() private addToWaitingList: EventEmitter<BookResponse> = new EventEmitter<BookResponse>();
+  @Output() private borrow: EventEmitter<BookResponse> = new EventEmitter<BookResponse>();
+  @Output() private edit: EventEmitter<BookResponse> = new EventEmitter<BookResponse>();
+  @Output() private details: EventEmitter<BookResponse> = new EventEmitter<BookResponse>();
+
+  onShowDetails() {
+    this.details.emit(this._book);
+  }
+
+  onBorrow() {
+    this.borrow.emit(this._book);
+  }
+
+  onAddToWaitingList() {
+  this.addToWaitingList.emit(this._book);
+  }
+
+  onEdit() {
+    this.edit.emit(this._book);
+  }
+
+  onShare() {
+    this.share.emit(this._book);
+  }
+
+  onArchive() {
+    this.archive.emit(this._book);
+  }
 }
