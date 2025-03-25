9a1a99e427d5fc8343692537bf407ad08f757c63
georgiivanov <gogata92@pm.me>
2025-03-25 17:42:21 +0100

    implemented Borrow book function

--- book-social-network/book-network-ui/src/app/pages/book/components/book-list/book-list.component.ts
+++ book-social-network/book-network-ui/src/app/pages/book/components/book-list/book-list.component.ts
@@ -1,16 +1,18 @@
 import {Component, OnInit} from '@angular/core';
 import {BookService} from '../../../../services/services/book.service';
 import {Router} from '@angular/router';
-import {BookResponse} from '../../../../services/models/book-response';
 import {PageResponseBookResponse} from '../../../../services/models/page-response-book-response';
-import {NgForOf} from '@angular/common';
+import {NgForOf, NgIf} from '@angular/common';
 import {BookCardComponent} from '../book-card/book-card.component';
+import {BookResponse} from '../../../../services/models/book-response';
+
 
 @Component({
   selector: 'app-book-list',
   imports: [
     NgForOf,
-    BookCardComponent
+    BookCardComponent,
+    NgIf
   ],
   templateUrl: './book-list.component.html',
   styleUrl: './book-list.component.scss'
@@ -20,13 +22,15 @@ export class BookListComponent implements OnInit {
 
   page: number = 0;
   size: number = 2;
+  message: string = '';
+  level: string = 'success';
 
   constructor(private bookService: BookService, private router: Router) {
   }
 
   ngOnInit(): void {
-        this.findAllBooks();
-    }
+    this.findAllBooks();
+  }
 
   private findAllBooks() {
     this.bookService.findAllBooks({
@@ -65,6 +69,25 @@ export class BookListComponent implements OnInit {
   }
 
   get isLastPage(): boolean {
-      return this.page == this.bookResponse.totalPages as number - 1;
+    return this.page == this.bookResponse.totalPages as number - 1;
+  }
+
+  borrowBook(book: BookResponse) {
+    this.message = '';
+    this.bookService.borrowBook({
+      'book-id': book.id as number,
+
+    }).subscribe({
+      next: book => {
+        this.level = 'success';
+        this.message = "Book successfully added to your list.";
+      },
+      error: (err) => {
+        console.log(err);
+        this.level = 'error';
+        this.message = err.error.error;
+      }
+
+    })
   }
 }
