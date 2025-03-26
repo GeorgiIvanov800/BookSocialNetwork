699333ce3ecad1472db6b06b695ba005166263b3
georgiivanov <gogata92@pm.me>
2025-03-26 17:30:04 +0100

    start creating the borrowed book  page

--- book-social-network/book-network-ui/src/app/pages/book/components/borrowed-book-list/borrowed-book-list.component.ts
+++ book-social-network/book-network-ui/src/app/pages/book/components/borrowed-book-list/borrowed-book-list.component.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NgForOf, NgIf} from '@angular/common';
+import {PageResponseBorrowedBookResponse} from '../../../../services/models/page-response-borrowed-book-response';
+import {BorrowedBookResponse} from '../../../../services/models/borrowed-book-response';
+import {BookService} from '../../../../services/services/book.service';
 
 @Component({
   selector: 'app-borrowed-book-list',
-  imports: [],
+  imports: [
+    NgForOf,
+    NgIf
+  ],
   templateUrl: './borrowed-book-list.component.html',
   styleUrl: './borrowed-book-list.component.scss'
 })
-export class BorrowedBookListComponent {
+export class BorrowedBookListComponent  implements OnInit {
+  page = 0;
+  size = 5;
 
+
+  constructor(private bookService: BookService) {
+  }
+
+  ngOnInit(): void {
+      this.findAllBorrowedBooks();
+  }
+  borrowedBooks: PageResponseBorrowedBookResponse = {};
+
+  returnBorrowedBook(book: BorrowedBookResponse) {
+
+  }
+
+  private findAllBorrowedBooks() {
+    this.bookService.findAllBorrowedBooks({
+      page: this.page,
+      size: this.size
+    }).subscribe({
+      next: resp => {
+        this.borrowedBooks = resp;
+      }
+    })
+  }
 }
