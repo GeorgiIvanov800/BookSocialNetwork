460112c4c90bb8e6f0717c2be5b6effc2e1704b5
georgiivanov <gogata92@pm.me>
2025-03-27 19:01:03 +0100

    implemented method for returned book

--- book-social-network/book-network-ui/src/app/pages/book/components/returned-books/returned-books.component.ts
+++ book-social-network/book-network-ui/src/app/pages/book/components/returned-books/returned-books.component.ts
@@ -1,11 +1,68 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NgForOf, NgIf} from '@angular/common';
+import {BookService} from '../../../../services/services/book.service';
+import {PageResponseBorrowedBookResponse} from '../../../../services/models/page-response-borrowed-book-response';
 
 @Component({
   selector: 'app-returned-books',
-  imports: [],
+  imports: [
+    NgForOf,
+    NgIf
+  ],
   templateUrl: './returned-books.component.html',
   styleUrl: './returned-books.component.scss'
 })
-export class ReturnedBooksComponent {
+export class ReturnedBooksComponent implements OnInit {
+  page = 0;
+  size = 5;
 
+  returnedBooks: PageResponseBorrowedBookResponse = {};
+
+  constructor(private bookService: BookService) {
+  }
+
+  ngOnInit(): void {
+    this.findAllReturnedBooks();
+  }
+
+
+  private findAllReturnedBooks() {
+    this.bookService.findAllReturnedBooks({
+      page: this.page,
+      size: this.size
+    }).subscribe({
+      next: resp => {
+        this.returnedBooks = resp;
+      }
+    })
+  }
+
+  goToFirstPage() {
+    this.page = 0;
+    this.findAllReturnedBooks();
+  }
+
+  GoToPreviousPage() {
+    this.page--;
+    this.findAllReturnedBooks();
+  }
+
+  GoToPage(page: number) {
+    this.page = page;
+    this.findAllReturnedBooks();
+  }
+
+  goToNextPage() {
+    this.page++;
+    this.findAllReturnedBooks();
+  }
+
+  GoToLastPage() {
+    this.page = this.returnedBooks.totalPages as number - 1;
+    this.findAllReturnedBooks();
+  }
+
+  get isLastPage(): boolean {
+    return this.page == this.returnedBooks.totalPages as number - 1;
+  }
 }
